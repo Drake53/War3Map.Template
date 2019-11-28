58acdd9e4ea257a97f32052aa08fe8ea4479e9b4
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-11-28 14:38:02 +0100

    Update packages.

--- War3Map.Template.Launcher/CompilerOptions.cs
+++ War3Map.Template.Launcher/CompilerOptions.cs
@@ -11,7 +11,7 @@ namespace War3Map.Template.Launcher
             var scriptCompilerOptions = new ScriptCompilerOptions(CSharpLua.CoreSystem.CoreSystemProvider.GetCoreSystemFiles());
 
             scriptCompilerOptions.MapInfo = Info.GetMapInfo();
-            scriptCompilerOptions.MapEnvironment = new MapEnvironment(War3Net.Build.Common.Tileset.LordaeronSummer, 65, 65);
+            scriptCompilerOptions.MapEnvironment = new MapEnvironment(War3Net.Build.Common.Tileset.LordaeronSummer, scriptCompilerOptions.MapInfo);
 
             scriptCompilerOptions.SourceDirectory = sourceDirectory;
             scriptCompilerOptions.OutputDirectory = outputDirectory;
